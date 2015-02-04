a5ffffe22b691cc9abdbe9ac87375c19c1769747
yuanqing <hello@yuanqing.sg>
2015-02-05 00:55:44 +0800

    Add tests for when `str` contains operators

--- test/unquoted.js
+++ test/unquoted.js
@@ -3,22 +3,93 @@
 var test = require('tape');
 var stoke = require('../');
 
-test('single token', function(t) {
-  t.looseEqual(stoke('foo'), [
-    { type: 'unquoted', body: 'foo' }
-  ]);
+test('without operators', function(t) {
+  [
+    [
+      'foo', [
+        { type: 'unquoted', body: 'foo' }
+      ]
+    ],
+    [
+      'foo bar', [
+        { type: 'unquoted', body: 'foo' },
+        { type: 'unquoted', body: 'bar' }
+      ]
+    ],
+    [
+      ' foo bar ', [
+        { type: 'unquoted', body: 'foo' },
+        { type: 'unquoted', body: 'bar' }
+      ]
+    ]
+  ].forEach(function(v) {
+    t.looseEqual(stoke(v[0]), v[1]);
+  });
   t.end();
 });
 
-test('multiple tokens', function(t) {
-  [ 'foo bar', ' foo bar ' ].forEach(function(v) {
-    t.looseEqual(stoke(v), [
-      { type: 'unquoted', body: 'foo' },
-      { type: 'unquoted', body: 'bar' }
-    ]);
+test('with operators', function(t) {
+
+  t.test('bare operators', function(t) {
+    [
+      [
+        '<', [
+          { type: 'unquoted', body: '<' }
+        ]
+      ],
+      [
+        '>', [
+          { type: 'unquoted', body: '>' }
+        ]
+      ],
+      [
+        '|', [
+          { type: 'unquoted', body: '|' }
+        ]
+      ],
+      [
+        ';', [
+          { type: 'unquoted', body: ';' }
+        ]
+      ]
+    ].forEach(function(v) {
+      t.looseEqual(stoke(v[0]), v[1]);
+    });
+    t.end();
   });
-  t.looseEqual(stoke('foo'), [
-    { type: 'unquoted', body: 'foo' }
-  ]);
-  t.end();
+
+  t.test('operators between tokens', function(t) {
+    [
+      [
+        'foo < bar > baz | bim ; bam', [
+          { type: 'unquoted', body: 'foo' },
+          { type: 'unquoted', body: '<' },
+          { type: 'unquoted', body: 'bar' },
+          { type: 'unquoted', body: '>' },
+          { type: 'unquoted', body: 'baz' },
+          { type: 'unquoted', body: '|' },
+          { type: 'unquoted', body: 'bim' },
+          { type: 'unquoted', body: ';' },
+          { type: 'unquoted', body: 'bam' }
+        ]
+      ],
+      [
+        'foo<bar>baz|bim;bam', [
+          { type: 'unquoted', body: 'foo' },
+          { type: 'unquoted', body: '<' },
+          { type: 'unquoted', body: 'bar' },
+          { type: 'unquoted', body: '>' },
+          { type: 'unquoted', body: 'baz' },
+          { type: 'unquoted', body: '|' },
+          { type: 'unquoted', body: 'bim' },
+          { type: 'unquoted', body: ';' },
+          { type: 'unquoted', body: 'bam' }
+        ]
+      ]
+    ].forEach(function(v) {
+      t.looseEqual(stoke(v[0]), v[1]);
+    });
+    t.end();
+  });
+
 });
